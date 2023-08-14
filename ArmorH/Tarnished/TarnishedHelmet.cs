a3243771079e5558cb058b1b0fef8707530c0ae4
BlockCrusader <141888765+BlockCrusader@users.noreply.github.com>
2023-08-14 11:52:41 -0400

    /Content uploads: Part 7 - items pt3

---
+++ ArmorH/Tarnished/TarnishedHelmet.cs
@@ -0,0 +1,75 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using ArtificerMod.Common;
+using ArtificerMod.Content.Glowmasks;
+using Microsoft.Xna.Framework.Graphics;
+using ArtificerMod.Content.Buffs.ArmorH;
+
+namespace ArtificerMod.Content.Items.ArmorH.Tarnished
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class TarnishedHelmet : ModItem
+	{
+		public override void SetStaticDefaults() {
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+
+			if (!Main.dedServ)
+			{
+				HeadLayer.RegisterData(Item.headSlot, new DrawLayerData()
+				{
+					Texture = ModContent.Request<Texture2D>("ArtificerMod/Content/Glowmasks/ArmorH/TarnishedHelmet_Glow")
+				});
+			}
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 16; 
+			Item.height = 16; 
+			Item.value = Item.buyPrice(0, 12, 50, 0); 
+			Item.defense = 10;
+			Item.rare = ItemRarityID.LightRed;
+		}
+
+		public override void UpdateEquip(Player player)
+		{
+			player.GetDamage(DamageClass.Generic) += 0.03f;
+			player.GetCritChance(DamageClass.Generic) += 3f;
+			player.GetModPlayer<ArtificerPlayer>().ammoCost95Helm = true;
+		}
+
+		
+		public override bool IsArmorSet(Item head, Item body, Item legs)
+		{
+			return body.type == ModContent.ItemType<TarnishedBreastplate>() && legs.type == ModContent.ItemType<TarnishedLeggings>();
+		}
+
+		public override void UpdateArmorSet(Player player)
+		{
+			player.setBonus = "+1 Artificer Accessory Slot" +
+				"\nAbility Accessory cooldowns are no longer extended" +
+				"\nIf you take fatal damage, protective souls will instantly restore you to 100 health" +
+				"\nThis revival effect has a 5 minute cooldown";
+			player.GetModPlayer<ArtificerPlayer>().tarnishedSetBonus = true;
+		}
+
+		public override void ArmorSetShadows(Player player)
+		{
+			player.armorEffectDrawShadow = true;
+			player.armorEffectDrawShadowSubtle = !player.HasBuff(ModContent.BuffType<TarnishedRevive>());
+		}
+
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+				.AddIngredient<PossessedHelmet>()
+				.AddIngredient(ItemID.SoulofNight, 12)
+				.AddIngredient(ItemID.PurificationPowder, 25)
+				.AddTile(TileID.MythrilAnvil)
+				.Register();
+		}
+	}
+}
---
+++ ArmorH/XenoVisor.cs
@@ -0,0 +1,79 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using ArtificerMod.Common;
+using ArtificerMod.Content.Items.Others;
+using ArtificerMod.Content.Glowmasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ArtificerMod.Content.Items.ArmorH
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class XenoVisor : ModItem
+	{
+		public override void SetStaticDefaults() {
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+
+			if (!Main.dedServ)
+			{
+				HeadLayer.RegisterData(Item.headSlot, new DrawLayerData()
+				{
+					Texture = ModContent.Request<Texture2D>("ArtificerMod/Content/Glowmasks/ArmorH/XenoVisor_Glow"),
+					ExtraTextureShield = ModContent.Request<Texture2D>("ArtificerMod/Content/Glowmasks/ArmorH/XenoVisor_Shield")
+				});
+			}
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 16; 
+			Item.height = 16; 
+			Item.value = Item.buyPrice(0, 24, 0, 0);
+			Item.defense = 13; 
+			Item.rare = ItemRarityID.Yellow;
+		}
+
+		public override void UpdateEquip(Player player)
+		{
+			player.GetDamage(DamageClass.Generic) += 0.03f;
+			player.GetCritChance(DamageClass.Generic) += 5f;
+			player.maxMinions++;
+			player.GetModPlayer<ArtificerPlayer>().ammoCost90Helm = true;
+		}
+
+		public override bool IsArmorSet(Item head, Item body, Item legs)
+		{
+			return body.type == ModContent.ItemType<XenoSuit>() && legs.type == ModContent.ItemType<XenoLeggings>();
+		}
+		public override void ArmorSetShadows(Player player)
+		{
+			int shieldCharge = player.GetModPlayer<ArtificerPlayer>().xenoShieldPower;
+			int shieldCap = player.GetModPlayer<ArtificerPlayer>().xenoShieldCap;
+			if (shieldCharge >= shieldCap)
+            {
+				player.armorEffectDrawOutlines = true;
+            }
+            else if(shieldCharge > 0)
+            {
+				player.armorEffectDrawOutlinesForbidden = true;
+            }
+		}
+
+		public override void UpdateArmorSet(Player player)
+		{
+			player.setBonus = "+2 Artificer Accessory Slots" +
+				"\nOver time, your armor will charge a temporarily energy shield that can absorb some damage"; 
+			player.GetModPlayer<ArtificerPlayer>().xenoSetBonus = true;
+		}
+
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+				.AddIngredient<MartianScrap>(5)
+				.AddIngredient(ItemID.MartianConduitPlating, 75)
+				.AddTile(TileID.MythrilAnvil)
+				.Register();
+		}
+	}
+}
